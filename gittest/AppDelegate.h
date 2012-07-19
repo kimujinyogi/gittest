46db6413ba8184bae87272cac8d6ea7698e3f5c4
JinHyuck Kim <kimujinyogi@gmail.com>
2012-07-20 00:27:35 +0900

    Initial Commit

---
+++ gittest/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  gittest
+//
+//  Created by JinHyuck Kim on 12/07/20.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ gittest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  gittest
+//
+//  Created by JinHyuck Kim on 12/07/20.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
